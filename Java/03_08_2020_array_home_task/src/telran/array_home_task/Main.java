03d2abf96772ba4d2d616373de272c31a6d9e0e4
DiBDV <bdv.dmitry@gmail.com>
2020-03-08 23:32:24 +0100

    Adding 'min' function to also show the smallest number the array

--- Java/03_08_2020_array_home_task/src/telran/array_home_task/Main.java
+++ Java/03_08_2020_array_home_task/src/telran/array_home_task/Main.java
@@ -7,6 +7,7 @@ public class Main {
         fillArray(arr);
         printArray(arr);
         returnMaxNumberInArray(arr);
+        returnMinNumberInArray(arr);
     }
 
     public static void fillArray(int [] array){
@@ -19,7 +20,7 @@ public class Main {
         for(int i = 0; i < array.length; i++){
 //            на самом деле предыдущий метод и этот метод похоже можно объединить,
 //            тогда получится что-то вроде:
-            public static void fillPrintArray(int [] array){
+//            public static void fillPrintArray(int [] array){
 //            for(int i = 0; i < array.length; i++){
 //                array[i] = (int)(Math.random()*20);
 //                System.out.println(array[i] + "");
@@ -31,14 +32,27 @@ public class Main {
     }
 
     public static void returnMaxNumberInArray(int[] array){
-//        предположим самы маленький элемент массива это ноль
+//        выставляем максимально значение цифры в массиве равное нулю
         int max = array[0];
-//        сравниваем значение 'max' с другими значениями в массиве
         for(int i = 0; i < array.length; i++){
             if(array[i] > max){
                 max = array[i];
             }
         }
+        System.out.println();
         System.out.println("The largest number in array is: " + max);
     }
+    public static void returnMinNumberInArray(int[] array){
+            //        выставляем минимальное значение цифры в массиве равное нулю
+            int min = array[0];
+//        сравниваем значение 'min' с другими значениями в массиве
+        for(int i = 0; i <array.length; i++){
+            if(array[i] < min){
+                min = array[i];
+            }
+        }
+        System.out.println();
+        System.out.println("The smallest number in array is: " + min);
+    }
+
 }
