820e79fef78385f6b4a21153528100fd7d5acda9
DiBDV <bdv.dmitry@gmail.com>
2020-03-31 20:58:47 +0200

    adding Plant project

---
+++ Java/03_31_2020_Java_plants/src/telran/data/Plant.java
@@ -0,0 +1,56 @@
+package telran.data;
+
+public abstract class  Plant {
+    private String name;
+    private int height;
+    private int age;
+
+    public Plant(String name, int height, int age) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+//    в поле возраст будем добавлять один год
+    public void addYearToAge(){
+        this.age ++ ;
+    }
+
+    public abstract int getGrowPerSeason();
+
+    public void doWinter(){
+        System.out.println(getName() + " is not growing in Winter " + getHeight());
+    }
+
+    public void doSpring(){
+        addYearToAge();
+        System.out.println(getName() + " at the beginning of the season the height is " + getHeight());
+        setHeight(getHeight() + getGrowPerSeason());
+        System.out.println(getName() + " has grown in Spring up to " + getHeight());
+    }
+
+    public abstract void doSummer();
+    public abstract void doAutumn();
+
+    @Override
+    public String toString() {
+        return "Plant{" +
+                "name='" + name + '\'' +
+                ", height=" + height +
+                ", age=" + age +
+                '}';
+    }
+}
